e6d279e8ae6cabbf5113ea387e96f097f0df81b8
MANISHPRASAD-1 <manishprasad987@gmail.com>
2025-05-14 03:03:33 +0530

    First Push

---
+++ Backend/blog-apps-apis/src/main/java/com/blooging/blog/BlogAppsApisApplication.java
@@ -0,0 +1,13 @@
+package com.blooging.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppsApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppsApisApplication.class, args);
+	}
+
+}
