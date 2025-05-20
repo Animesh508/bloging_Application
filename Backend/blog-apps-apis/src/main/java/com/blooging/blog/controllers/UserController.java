06a413d86b6a34827a39550cc17588fee9b4a954
MANISHPRASAD-1 <manishprasad987@gmail.com>
2025-05-20 13:03:02 +0530

    Full user logic complited update

--- Backend/blog-apps-apis/src/main/java/com/blooging/blog/controllers/UserController.java
+++ Backend/blog-apps-apis/src/main/java/com/blooging/blog/controllers/UserController.java
@@ -1,43 +1,69 @@
 package com.blooging.blog.controllers;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.blooging.blog.payloads.ApiResponce;
 import com.blooging.blog.payloads.UserDto;
 import com.blooging.blog.services.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
-	//POST-CREATE USER
+
+	// POST-CREATE USER
 	// created UserDto to not directely expose User
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
 		UserDto createdUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
 	}
-	
-	//PUT - UPDATE USER
-	@PutMapping("/")
-	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto){
+
+	// PUT - UPDATE USER
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> UpdateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer userId) {
+		UserDto updateUserDto = this.userService.updateUser(userDto, userId);
+		return ResponseEntity.ok(updateUserDto);
+
+	}
+
+	// DELETE - DELETE USER
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponce> DeleteUser(@PathVariable("userId") Integer userId) {
+		this.DeleteUser(userId);
+		return new ResponseEntity<ApiResponce>(new ApiResponce("user deleated ScuccessFully", true), HttpStatus.OK);
+
+	}
+
+	// GET - GET USER
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUser(){
 		
+		return ResponseEntity.ok(this.userService.getAllUser());
 		
 	}
 	
-	//DELETE - DELETE USER
-	
-	//GET - GET USER
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getSingleUser(@PathVariable("userId") Integer userId){
+		
+		return ResponseEntity.ok(this.userService.getUserById(userId));
+		
+	}
 
 }
